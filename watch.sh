b4a22ec1c12d4bb8553f5c71c1eacd58df936d44
Hazal Şule <hsule0112@gmail.com>
2025-06-03 19:39:29 +0300

    Auto commit on save

--- watch.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-while true; do
-    inotifywait -e modify -r ./  # Linux içindir, macOS için `fswatch .`
-    git add .
-    git commit -m "Auto commit on save"
-    git push origin main
-done
